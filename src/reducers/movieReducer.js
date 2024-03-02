1a83d84f1586103aa4cecb2ef6b14aca7617f625
Ashish Desai <desai.ashish76@gmail.com>
2024-03-02 16:44:16 -0500

    completed movieList changes

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -6,8 +6,10 @@ const initialState = {
     appTitle: "IMDB Movie Database"
 }
 
-const reducer = (state, action) => {
+const reducer = (state = initialState, action) => {
     switch(action.type) {
+        // case ADD_MOVIE:
+        //     return {...movies, newMovie: action.payload}
         case DELETE_MOVIE:
             return {
                 movies: state.movies.filter(item=>(action.payload !== item.id))
